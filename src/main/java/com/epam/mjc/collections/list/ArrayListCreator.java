0d922983d3516bc7abcdea5deb18c85980cc1a9e
Renata Yermak <renatkahturo@gmail.com>
2023-04-11 17:24:25 +0300

    .

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -4,6 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListCreator {
-    public ArrayList<String> createArrayList(List<String> sourceList) {
+    public  ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> result = new ArrayList<>();
+        for (int i = 2; i < sourceList.size(); i += 3) {
+            result.add(sourceList.get(i));
+            result.add(sourceList.get(i));
+        }
+        return result;
     }
 }
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -5,11 +5,21 @@ import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        sourceList.sort(new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        int firstString = Integer.parseInt(a);
+        int secondString = Integer.parseInt(b);
+        int result = Integer.compare(function(firstString), function(secondString));
+
+        return result != 0 ? result : Integer.compare(firstString, secondString);
+    }
+
+    private int function(int x) {
+        return (int) (5 * Math.pow(x, 2) + 3);
     }
 }
